6e2cc60fbfb7da126c5329939861bd5a38a2bdd2
sohan2410 <bandarysohan24@gmail.com>
2023-08-28 23:43:41 +0530

    added receive threads

--- client.py
+++ client.py
@@ -2,6 +2,11 @@ import socket
 import select
 import errno
 import sys
+from _thread import *
+import threading
+
+print_lock = threading.Lock()
+
 HEADER_LENGTH = 10
 
 IP = "127.0.0.1"
@@ -16,18 +21,13 @@ client_socket.setblocking(False)
 
 username = my_username.encode('utf-8')
 username_header = f"{len(username):<{HEADER_LENGTH}}".encode('utf-8')
-client_socket.send(username_header+username)
+client_socket.send(username_header + username)
 
-while True:
-    message = input(f'{my_username} > ')
-    if message:
-        message = message.encode('utf-8')
-        message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
-        client_socket.send(message_header + message)
-    try:
-        while True:
+
+def on(client_socket):
+    while True:
+        try:
             username_header = client_socket.recv(HEADER_LENGTH)
-            print('username_header', username_header)
             if not len(username_header):
                 print('Connection closed by the server')
                 sys.exit()
@@ -36,14 +36,27 @@ while True:
             message_header = client_socket.recv(HEADER_LENGTH)
             message_length = int(message_header.decode('utf-8').strip())
             message = client_socket.recv(message_length).decode('utf-8')
-            print(f'{username} > {message}')
+            with print_lock:
+                print('\n' + f'{username} > {message}')
 
-    except IOError as e:
-        if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
-            print('Reading error: {}'.format(str(e)))
+        except IOError as e:
+            if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+                print('Reading error: {}'.format(str(e)))
+                sys.exit()
+            # continue
+
+        except Exception as e:
+            print('Reading error: '.format(str(e)))
             sys.exit()
-        continue
 
-    except Exception as e:
-        print('Reading error: '.format(str(e)))
-        sys.exit()
+
+start_new_thread(on, (client_socket,))
+
+while True:
+    message = input(f'{my_username} > ')
+    if message:
+        temp = message.split(' ')
+
+        message = message.encode('utf-8')
+        message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
+        client_socket.send(message_header + message)
